16fa9971c7092f8b9938520a1c5bb3efaddb8a46
MS269 <59796004+MS269@users.noreply.github.com>
2021-04-24 01:22:00 +0900

    Updated challenge day 5 clock.js

--- challenge/day5/src/clock.js
+++ challenge/day5/src/clock.js
@@ -1,4 +1,4 @@
-const xmasDate = new Date("2021-12-24:00:00:00+0900").getTime();
+const xmasDate = new Date("2021-12-24:00:00:00+0900");
 
 const clockContainer = document.querySelector(".js-clock");
 const clockTime = clockContainer.querySelector("h3");
@@ -9,9 +9,13 @@ function formatTime(time) {
 }
 
 function getTime() {
-  const now = new Date().getTime();
+  // get korea time
+  const now = new Date();
+  const utcNow = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
+  const kstOffset = 9 * 60 * 60 * 1000;
+  const kstNow = new Date(utcNow + kstOffset);
 
-  const distance = xmasDate - now;
+  const distance = xmasDate.getTime() - kstNow.getTime();
   const d = Math.floor(distance / (1000 * 60 * 60 * 24));
   const h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
